d93cbbe13e3fdd67ed8cdfdbd737894b5ccf75af
sivamani <siva.mv@promantia.com>
2023-07-03 10:51:12 +0530

    Done with all the requirements

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -2,7 +2,58 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Airplane Ticket', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+        frm.page.set_primary_action("Submit", function() {
+            if (frm.doc.status !== "Boarded") {
+                frappe.throw("Cannot submit the Airplane Ticket document. Status must be 'Boarded'.");
+                return;
+            } else{
+                frm.save('Submit');
+			}
+        });
+    },
+	refresh: function(frm) {
+		frm.add_custom_button('Assign Seat', () => {
+			let dialogue = new frappe.ui.Dialog({
+				title: 'Assign seat',
+				fields: [
+					{
+						label: 'Seat',
+						fieldname: 'seat',
+						fieldtype: 'Data'
+					}
+				],
+				size:'small',
+				primary_action_label: 'Assign',
+				primary_action(values) {
+					frm.set_value('seat', values['seat']);
+					dialogue.hide();
+					frm.save();
+				}
+			});
+	
+			dialogue.show();
+		},('Action'))
+	},
 
-	// }
+	flight: function(frm) {
+
+		frappe.call({
+			method: "airplane_mode.airplane_mode.doctype.airplane_ticket.airplane_ticket.ticket_count",
+			args : {
+				flight : frm.doc.flight
+			},
+		})
+		.then((r) =>{
+
+			if (frm.doc.name.slice(0, -15) != frm.doc.flight){
+
+				if (r.message[0] === r.message[1]){
+					frappe.throw("All seats are booked..!!")
+				}
+			}
+		})
+	}
+	
+	
 });
--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -1,8 +1,58 @@
 # Copyright (c) 2023, Siva manikanta vemana and contributors
 # For license information, please see license.txt
 
-# import frappe
+import random
+import frappe
 from frappe.model.document import Document
 
 class AirplaneTicket(Document):
-	pass
+	# def autoname(self):
+	# 	row = random.randint(1, 30)
+	# 	letter = random.choice(['A','B','C','D','E'])
+	# 	if not self.seat:
+	# 		seat_number = generate_seat_number(row,letter)
+	# 		self.seat = seat_number
+
+
+	# def before_save(self):
+	# 	print(self)
+	# def total_amount(self):
+	# 	flight_price = self.flight_price
+	# 	add_onsp=0
+	# 	for item in self.add_ons:
+	# 		add_onsp +=add_ons.amount
+
+	# 	if flight_price and add_onsp:
+	# 		self.total_amount = flight_price + add_onsp
+	# 	else:
+	# 		self.total_amount = flight_price
+	
+	def calculate_total_amount(self):
+		flight_price = self.flight_price
+		add_ons_total = sum(add_on.amount for add_on in self.add_ons)
+		
+		if flight_price and add_ons_total:
+			self.total_amount = flight_price + add_ons_total
+		else:
+			self.total_amount = flight_price
+
+	def validate(self):
+		self.calculate_total_amount()
+			
+# def generate_seat_number(row,letter):
+# 	row = random.randint(1, 30)
+# 	letter = random.choice(['A', 'B', 'C', 'D', 'E', 'F'])
+# 	return f"{row}{letter}"
+
+@frappe.whitelist()
+def ticket_count(flight):
+
+	capacity=frappe.db.get_value(
+		"Airplane", filters={"name": flight[:-14]}, fieldname="capacity")
+		
+	ticket_list= len(frappe.db.get_list(
+		"Airplane Ticket", filters={"flight":flight}))
+	
+	return [capacity,ticket_list]
+
+
